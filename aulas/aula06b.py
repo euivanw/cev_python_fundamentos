599b16b271ba9ae9f9738fe8882d4cecdea3f491
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-01-30 13:17:56 -0300

    Aula 6 + Desafios

---
+++ aulas/aula06b.py
@@ -0,0 +1,9 @@
+n = float(input('Digite um valor: '))
+print(n)
+
+n2 = input('Digite uma string: ')
+print(n2.isnumeric())
+print(n2.isalpha())  # se é afalbético
+print(n2.isalnum())  # se é alfanumérico
+print(n2.isupper())  # se a string está totalmente em caixa alta
+print(n2.islower())  # se a string está totalmente em caixa baixa
---
+++ desafios/des004.py
@@ -0,0 +1,18 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis
+# sobre ele.
+
+string = input('Digite algo: ')
+
+print('Tipo primitivo: {}'.format(type(string)))
+print('É alfabético: {}'.format(string.isalpha()))
+print('Está em caixa alta: {}'.format(string.isupper()))
+print('Está em caixa baixa: {}'.format(string.islower()))
+print('É alfanumérico: {}'.format(string.isalnum()))
+print('É numérico: {}'.format(string.isnumeric()))
+print('É ASCII: {}'.format(string.isascii()))
+print('É decimal: {}'.format(string.isdecimal()))
+print('É um dígito: {}'.format(string.isdigit()))
+print('É um identificador: {}'.format(string.isidentifier()))
+print('É um imprimível: {}'.format(string.isprintable()))
+print('É um espaço: {}'.format(string.isspace()))
+print('Está no formato de título: {}'.format(string.istitle()))
