ab4cda9753fcdb0e0f42209406e60dd5bc4d4c3c
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-02 14:51:01 -0300

    Exercício 005

---
+++ exercicios/ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+
+numero = int(input('Digite um número inteiro: '))
+
+print(f'Número lido: {numero}')
+print(f'Antecessor: {numero - 1}')
+print(f'Sucessor: {numero + 1}')
