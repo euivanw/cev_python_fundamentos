5e389b7ce0c68207471b91b7010b6ad8a9ebbf72
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-02 15:05:30 -0300

    Exercício 009

---
+++ exercicios/ex009.py
@@ -0,0 +1,14 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+numero = int(input('Digite um número inteiro.: '))
+
+print('{:5} x  1 = {:5}'.format(numero, numero * 1))
+print('{:5} x  2 = {:5}'.format(numero, numero * 2))
+print('{:5} x  3 = {:5}'.format(numero, numero * 3))
+print('{:5} x  4 = {:5}'.format(numero, numero * 4))
+print('{:5} x  5 = {:5}'.format(numero, numero * 5))
+print('{:5} x  6 = {:5}'.format(numero, numero * 6))
+print('{:5} x  7 = {:5}'.format(numero, numero * 7))
+print('{:5} x  8 = {:5}'.format(numero, numero * 8))
+print('{:5} x  9 = {:5}'.format(numero, numero * 9))
+print('{:5} x 10 = {:5}'.format(numero, numero * 10))
