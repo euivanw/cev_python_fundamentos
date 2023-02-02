046561041aefa519f9a5579115d2e656137b879c
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-02 14:58:28 -0300

    Exercício 008

---
+++ exercicios/ex008.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+valor_em_metros = float(input('Digite um valor em metros.: '))
+
+print(f'Valor em metros...........: {valor_em_metros}')
+print(f'Valor em centímetros......: {valor_em_metros * 100}')
+print(f'Valor em milímetros.......: {valor_em_metros * 1000}')
