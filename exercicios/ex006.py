28e7cbd9c322f14f6bff5789d83048abee54f091
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-02 14:53:23 -0300

    Exercício 006

---
+++ exercicios/ex006.py
@@ -0,0 +1,8 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e a raiz quadrada.
+
+numero = int(input('Digite um número inteiro: '))
+
+print(f'Número digitado: {numero}')
+print(f'Dobro: {numero * 2}')
+print(f'Triplo: {numero * 3}')
+print(f'Raiz Quadrada: {numero ** (1 / 2)}')
