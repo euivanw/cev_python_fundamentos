a04eda3eacc0faced43c3ba0eff38281d7785c85
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-02 14:55:20 -0300

    Exercício 007

---
+++ exercicios/ex007.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota.: '))
+
+print(f'Nota 1: {nota1}')
+print(f'Nota 2: {nota2}')
+print(f'Média.: {(nota1 + nota2) / 2}')
