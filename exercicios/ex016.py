4bbf0cb4a7355d6e32516c2739f7746cf837854f
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-02-03 12:03:31 -0300

    Exercício 016

---
+++ exercicios/ex016.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+# Ex:
+#    Digite um número: 6.127
+#    O número 6.1274 tem a parte inteira 6.
+from math import trunc
+
+numero = float(input('Digite um número.: '))
+inteiro = trunc(numero)
+
+print('A parte inteira do número {} é igual a {}.'.format(numero, inteiro))
