c7f6b8c5eade5419a7102ced3acf522bc8b6be95
Ivan Wilhelm (ele/dele) <contato@ivanwilhelm.dev>
2023-02-04 13:49:01 -0300

    Exercício 024

---
+++ exercicios/ex024.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome de uma cidade e digita se ela começa ou não com o nome 'Santo'
+
+cidade = input('Digite o nome de uma cidade.: ')
+
+print('O nome da cidade começa com Santo? R: {}'.format(cidade.upper().startswith('SANTO')))
