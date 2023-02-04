991df7f3423ad6e1047a47595a8b31f3ca4d41e8
Ivan Wilhelm (ele/dele) <contato@ivanwilhelm.dev>
2023-02-04 13:50:52 -0300

    Exercício 025

---
+++ exercicios/ex025.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem SILVA no nome.
+
+nome = input('Digite um nome completo.: ')
+
+print('O nome da pessoa contém SILVA? R: {}'.format('SILVA' in nome.upper()))
