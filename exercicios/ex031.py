83ac8a62b0e4d2879977436bfa79dd6d748ec8d4
Ivan Wilhelm (ele/dele) <contato@ivanwilhelm.dev>
2023-02-06 15:24:33 -0300

    Exercício 31

---
+++ exercicios/ex031.py
@@ -0,0 +1,11 @@
+# Desenvolva um programa que pergunte a distância de uma viagem em Km.
+# Calcule o preço da passagem, cobrando R$ 0,50 por Km para viagens até 200Km e R$ 0,45 para viagens mais longas.
+
+distancia = float(input('Digite a distância da viagem em Km: '))
+
+if (distancia <= 200.0):
+    passagem = distancia * 0.5
+else:
+    passagem = distancia * 0.45
+
+print('Preço da passagem R$ {:.2f}'.format(passagem))
