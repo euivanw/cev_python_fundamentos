0ab601d898949ecfde0e50c6fcae5c4bb03b5aea
Ivan Wilhelm (ele/dele) <contato@ivanwilhelm.dev>
2023-02-06 15:16:17 -0300

    Exercício 028

---
+++ exercicios/ex028.py
@@ -0,0 +1,13 @@
+# Escreva um programa que faça o computador pensar em número entre 0 e 5 peça ao usuário tentar descobrir qual foi o
+# número escolhido pelo computador.
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+
+numero = int(input('Digite um número entre 0 e 5.: '))
+numero_sorteado = randint(0, 5)
+
+if (numero == numero_sorteado):
+    print('Parabéns, você ganhou!')
+else:
+    print('Você perdeu, o número sorteado foi {}.'.format(numero_sorteado))
